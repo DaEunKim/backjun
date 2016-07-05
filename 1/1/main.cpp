85174ebd621ee5e94ace90f7820b6974b5c43cfc
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-07-05 10:44:04 +0900

    1

---
+++ 1/1/main.cpp
@@ -0,0 +1,22 @@
+//
+//  number.cpp
+//  Summer
+//
+//  Created by 김다은 on 2016. 7. 5..
+//  Copyright © 2016년 김다은. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+
+int main(void){
+    int N;
+    cin >> N;
+    for(int i=N;i>0;i--){
+        cout << i << endl;
+    }
+}
---
+++ 2/2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2
+//
+//  Created by 김다은 on 2016. 7. 5..
+//  Copyright © 2016년 김다은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
