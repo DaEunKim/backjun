c2cd0d95bc25c0da3252f1effb9f312ecc5e1b3b
DaEunKim <Dani@gimda-eunui-MacBook-Air.local>
2016-07-06 23:41:15 +0900

    .

--- 2/2/main.cpp
+++ 2/2/main.cpp
@@ -1,15 +1,30 @@
 //
-//  main.cpp
-//  2
+//  number.cpp
+//  Summer
 //
 //  Created by 김다은 on 2016. 7. 5..
 //  Copyright © 2016년 김다은. All rights reserved.
 //
 
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+int main(void){
+    int N;
+    int count=0;
+    cin >> N;
+    for(int i=0;i<5;i++){
+        int a;
+        cin>>a;
+        if(N==a){
+            count++;
+        }
+
+    }
+    cout<< count<<endl;
+    
 }
