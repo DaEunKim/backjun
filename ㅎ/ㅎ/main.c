736a963399e80ce7cc2c91db88e1a71f505d1cbb
DaEunKim <ekdms717@naver.com>
2016-08-06 23:37:01 +0900

    ㅇ

---
+++ ㅎ/ㅎ/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  ㅎ
+//
+//  Created by 김다은 on 2016. 7. 10..
+//  Copyright © 2016년 김다은. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 12;
+    int num2 = 10;
+    int result1, result2, result3;
+    
+    result1 = (num1 == 12 && num2 == 10);
+    return 0;
+}
